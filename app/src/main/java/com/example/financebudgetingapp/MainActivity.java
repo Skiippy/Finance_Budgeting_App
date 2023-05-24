1fb20ba4504f8358b96eb47a9551bbd0c1cb5d90
Skiippy <skiippygaming@gmail.com>
2023-05-24 14:26:07 +0200

    Added page linking between signup and questionnaire page

--- app/src/main/java/com/example/financebudgetingapp/MainActivity.java
+++ app/src/main/java/com/example/financebudgetingapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.financebudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+private  androidx.appcompat.widget.AppCompatButton btnSignup;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
+
+        btnSignup = (AppCompatButton) findViewById(R.id.buttonSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuestionaire();
+            }
+        });
+    }
+
+    public void openQuestionaire() {
+        Intent intent = new Intent(this, Questionaire.class);
     }
 }
