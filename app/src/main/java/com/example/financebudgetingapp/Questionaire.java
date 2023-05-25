3234963dfdde3daeb8665ee5609a4dd4adda3e55
KiyanMckop <kiyanmckop395@gmail.com>
2023-05-25 09:45:52 +0200

    Created new SupportPage.java added arrow images corrected code for collapsible textViews

--- app/src/main/java/com/example/financebudgetingapp/Questionaire.java
+++ app/src/main/java/com/example/financebudgetingapp/Questionaire.java
@@ -2,7 +2,10 @@ package com.example.financebudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Questionaire extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class Questionaire extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quesrtionnaire_page);
+
+        Button btnSubmit = (Button) findViewById(R.id.btnQuestionnaireSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSupportPage();
+            }
+        });
+
     }
+
+    public void startSupportPage(){
+        Intent intent = new Intent(this, SupportPage.class);
+        startActivity(intent);
+    }
+
 }
