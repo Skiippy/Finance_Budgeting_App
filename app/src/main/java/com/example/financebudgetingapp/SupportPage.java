3a2e57073aca750935775aa1870751d65cccf5af
KiyanMckop <kiyanmckop395@gmail.com>
2023-05-25 09:47:05 +0200

    Created new SupportPage.java added arrow images corrected code for collapsible textViews

--- app/src/main/java/com/example/financebudgetingapp/SupportPage.java
+++ app/src/main/java/com/example/financebudgetingapp/SupportPage.java
@@ -1,29 +1,47 @@
 package com.example.financebudgetingapp;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.ImageButton;
-        import android.widget.TextView;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SupportPage extends AppCompatActivity {
 
-    ImageButton expandText = (ImageButton) findViewById(R.id.btnExpandText);
-    TextView descriptionText = (TextView) findViewById(R.id.descriptionText);
+    boolean expanded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.support_page);
 
-        expandText.setOnClickListener(new View.OnClickListener() {
+        @SuppressLint("UseCompatLoadingForDrawables") Drawable arrow_left_image = getResources().getDrawable(R.drawable.arrow_left);
+        @SuppressLint("UseCompatLoadingForDrawables") Drawable arrow_downwards_image = getResources().getDrawable(R.drawable.arrow_downwards);
+
+        ImageButton IbtnExpandText = (ImageButton) findViewById(R.id.btnExpandText);
+        TextView descriptionText = (TextView) findViewById(R.id.descriptionText);
+
+        IbtnExpandText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                descriptionText.setMaxLines(Integer.MAX_VALUE);
+                if (!expanded) {
+                    descriptionText.setMaxLines(Integer.MAX_VALUE);
+                    IbtnExpandText.setImageDrawable(arrow_left_image);
+                    expanded = true;
+                }else{
+                    descriptionText.setLines(3);
+                    IbtnExpandText.setImageDrawable(arrow_downwards_image);
+                    expanded = false;
+                }
             }
         });
 
     }
-
 }
