a1252f0cdf0441cab02c863b1dcca46a3a012985
TyronBarnard <tyronbarnard12345@gmail.com>
2023-05-28 15:03:53 +0200

    Goal Page Navigation - Home + Profile + Support + Updated Progress Bars

--- app/src/main/java/com/example/financebudgetingapp/GoalsPage.java
+++ app/src/main/java/com/example/financebudgetingapp/GoalsPage.java
@@ -2,7 +2,10 @@ package com.example.financebudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class GoalsPage extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class GoalsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goals_page);
+
+        ImageButton ibProfileButton = findViewById(R.id.ibProfileButton);
+        ImageView btnSupportPage = findViewById(R.id.btnSupportPage);
+        ImageView btnHomePage = findViewById(R.id.btnHomePage);
+
+        ibProfileButton.setOnClickListener(v -> startProfilePage());
+        btnSupportPage.setOnClickListener(v -> startSupportPage());
+        btnHomePage.setOnClickListener(v -> startOverviewPage());
+
     }
+
+    public void startSupportPage(){
+        Intent intent = new Intent(this, SupportPage.class);
+        startActivity(intent);
+    }
+    public void startProfilePage() {
+        startActivity(new Intent(this, ProfilePage.class));
+
+    }
+    public void startOverviewPage(){
+            startActivity(new Intent(this, OverviewPage.class));
+    }
+
 }
