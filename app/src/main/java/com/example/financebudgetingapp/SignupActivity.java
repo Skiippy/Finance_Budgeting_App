05190b12b6aa59f429fb9902ee91e3dae03c7977
Skiippy <skiippygaming@gmail.com>
2023-06-20 14:07:42 +0200

    Added Input Validation to Signup

--- app/src/main/java/com/example/financebudgetingapp/SignupActivity.java
+++ app/src/main/java/com/example/financebudgetingapp/SignupActivity.java
@@ -29,6 +29,8 @@ public class SignupActivity extends AppCompatActivity {
         EditText edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
         TextView loginText = findViewById(R.id.loginText);
 
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+
         Button buttonSignup = findViewById(R.id.buttonSignup);
 
         buttonSignup.setOnClickListener(new View.OnClickListener() {
@@ -40,25 +42,33 @@ public class SignupActivity extends AppCompatActivity {
                 if (email.equals("") || password.equals("") || confirmPassword.equals(""))
                     Toast.makeText(SignupActivity.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
                 else {
-                    if (password.equals(confirmPassword)) {
-                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
+                    if (!email.matches(emailRegex))
+                        Toast.makeText(SignupActivity.this, "Email Format is Invalid", Toast.LENGTH_SHORT).show();
+                    else {
+                        if (password.length() < 8)
+                            Toast.makeText(SignupActivity.this, "Password has to be 8 Characters or Longer", Toast.LENGTH_SHORT).show();
+                        else {
+                            if (password.equals(confirmPassword)) {
+                                Boolean checkUserEmail = databaseHelper.checkEmail(email);
 
-                        if (checkUserEmail == false) {
-                            Boolean insert = databaseHelper.insert(email, password);
+                                if (checkUserEmail == false) {
+                                    Boolean insert = databaseHelper.insert(email, password);
 
-                            if (insert == true) {
-                                Toast.makeText(SignupActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), Questionaire.class);
-                                intent.putExtra("userEmail", edtEmail.getText().toString());
-                                startActivity(intent);
+                                    if (insert == true) {
+                                        Toast.makeText(SignupActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Questionaire.class);
+                                        intent.putExtra("userEmail", edtEmail.getText().toString());
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, "Signup Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(SignupActivity.this, "User already exists, Please Login", Toast.LENGTH_SHORT).show();
+                                }
                             } else {
-                                Toast.makeText(SignupActivity.this, "Signup Failed", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignupActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
                             }
-                        } else {
-                            Toast.makeText(SignupActivity.this, "User already exists, Please Login", Toast.LENGTH_SHORT).show();
                         }
-                    } else {
-                        Toast.makeText(SignupActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
